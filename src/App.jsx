8a6039f321ab2b3c9de7edeff0381ce147d87f79
christopherMills <christopher.mills.github@outlook.com>
2019-10-28 19:26:54 +0000

    simple router created

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// MODULES
+import React from 'react';
+import { Router } from '@reach/router';
+// STYLES
+import './App.css';
+// COMPONENTS
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+// MAIN
+class App extends React.Component {
+  state = {};
+  render() {
+    <div className='App'>
+      <Header />
+      <Router>
+        <HomePage path='/' />
+      </Router>
+    </div>;
+  }
+}
+
+//EXPORTS
+export default App;
