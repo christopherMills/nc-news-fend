467d71685546945d7d8469035b809b6c9847fd74
christopherMills <christopher.mills.github@outlook.com>
2019-10-30 22:15:13 +0000

    overhauled for conditional rendering of sort-by drop-down, and assocaited functions for passing user sort-by choices up to App.jsx

--- src/components/main/NavBar.jsx
+++ src/components/main/NavBar.jsx
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from '@reach/router';
 import './NavBar.css';
 
-const NavBar = (props) => {
-  return (
-    <nav className='navBar'>
-      <Link to='/articles'>
-        <button>Articles</button>
-      </Link>
-      <Link to='/topics'>
-        <button>Topics</button>
-      </Link>
-      <Link to='/authors'>
-        <button>Authors</button>
-      </Link>
-    </nav>
-  );
-};
+export default class NavBar extends Component {
+  state = {
+    sort_by: undefined,
+    order: undefined,
+  };
 
-export default NavBar;
+  // HELPER FNS
+
+  // tells App.jsx to update its state for the
+  // SQL query parameters in ArticlesList etc
+  updateSortParams(sort_by, order) {
+    this.props.setSortParams(sort_by, order);
+  }
+
+  // handle user clicking new sort-by option
+  handleChange = (event) => {
+    this.setState({
+      sort_by: event.target.value,
+    });
+  };
+
+  // handle the form being submitted
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  // LIFECYCLE
+
+  componentDidMount() {
+    this.setState({
+      sort_by: this.props.sort_by,
+      order: this.props.order,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { sort_by, order } = this.state;
+    if (prevProps !== this.props) {
+      this.render();
+    }
+    if (prevState.sort_by !== sort_by) {
+      this.props.setSortParams(sort_by, order);
+    }
+  }
+
+  // RENDER
+
+  render() {
+    const { displaySort } = this.props;
+    return (
+      <nav className='navBar'>
+        <Link to='/articles'>
+          <button>Articles</button>
+        </Link>
+        <Link to='/topics'>
+          <button>Topics</button>
+        </Link>
+        <Link to='/authors'>
+          <button>Authors</button>
+        </Link>
+        {displaySort && (
+          <form onSubmit={this.handleSubmit}>
+            <select value={this.state.sort_by} onChange={this.handleChange}>
+              <option value='created_at'>Date / time</option>
+              <option value='comment_count'>Comments</option>
+              <option value='votes'>Vote count</option>
+            </select>
+          </form>
+        )}
+      </nav>
+    );
+  }
+}
