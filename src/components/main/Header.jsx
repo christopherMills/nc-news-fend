83afb7a9ae3541e9601efcf3144ff9c4fb4c432f
christopherMills <christopher.mills.github@outlook.com>
2019-10-30 22:12:18 +0000

    Turned Header into a home link

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import './Header.css';
 
 const Header = () => {
   return (
-    <div className='mainHeader'>
+    <Link className='mainHeader' to='/'>
       <h1>NCNews</h1>
-    </div>
+    </Link>
   );
 };
 
