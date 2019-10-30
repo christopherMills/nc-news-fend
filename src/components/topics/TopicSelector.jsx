deb1c94463116f1f8449996e467c24e10c5b4030
christopherMills <christopher.mills.github@outlook.com>
2019-10-30 12:56:18 +0000

    add topic selector buttons as a seperate component

---
+++ src/components/topics/TopicSelector.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TopicSelector = ({ topicSel, topicList }) => {
+  return topicList.length ? (
+    <div id='topicSelectorMain'>
+      <p>Please select a topic:</p>
+      {topicList.map((eachTopic) => {
+        return (
+          <button
+            onClick={() => {
+              topicSel(eachTopic.slug);
+            }}
+            key={eachTopic.slug}>
+            {eachTopic.slug}
+          </button>
+        );
+      })}
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default TopicSelector;
