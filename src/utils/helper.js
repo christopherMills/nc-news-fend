e0ddf6eb4d5c0763ed06707dfa3a7197d8beba44
christopherMills <christopher.mills.github@outlook.com>
2019-10-31 08:32:59 +0000

    add a fn for shorter dates

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,12 @@
 // prettify a date / time
 // TAKES A JAVASCRIPT DATE OBJECT
 export const prettifyDate = (jsDateObj) => {
-  return jsDateObj === 'no date' ? jsDateObj : jsDateObj.toString();
+  return jsDateObj === 'no date' ? 'no date' : jsDateObj.toString();
 };
+
+export const getShortDate = (str) => {
+  const date = new Date(str);
+  return date.toLocaleString('default');
+};
+
+//              2017-12-24T05:38:51.240Z
