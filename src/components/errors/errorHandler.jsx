f6244cbf8b715b973a65b006f3a4bf4232884daa
christopherMills <christopher.mills.github@outlook.com>
2019-11-01 14:31:41 +0000

    refactored into a functional component

--- src/components/errors/errorHandler.jsx
+++ src/components/errors/errorHandler.jsx
@@ -1,23 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class ErrorHandler extends Component {
-  state = {
-    errorStatusCode: undefined,
-    errorMsg: undefined,
-  };
-  componentDidMount = () => {
-    this.setState({
-      errorStatusCode: this.props.statusCode,
-      errorMsg: this.props.errorMsg,
-    });
-  };
-  render() {
-    const { errorStatusCode, errorMsg } = this.state;
-    return (
-      <>
-        <h1>Error (status code {errorStatusCode})</h1>
-        <p>{`Message from server: ${errorMsg}`}</p>
-      </>
-    );
-  }
+export default function ErrorHandler(props) {
+  const { statusCode, errorMsg } = props;
+  return (
+    <>
+      <h1>Error (status code {statusCode})</h1>
+      <p>{`Message from server: ${errorMsg}`}</p>
+    </>
+  );
 }
